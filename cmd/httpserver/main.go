e1144542dba47308a5e11fc55662b7dd996f7964
Tom George <tg82490@gmail.com>
2020-01-27 12:46:07 -0500

    Initial commit
    Signed-off-by: Tom George <tg82490@gmail.com>

---
+++ cmd/httpserver/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Printf("Hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+
+	logger, _ := zap.NewProduction()
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	logger.Info("Added Handlers")
+
+	logger.Info("Starting to serve")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tomgeorge/go-test-http-server
+
+go 1.13
+
+require go.uber.org/zap v1.13.0
